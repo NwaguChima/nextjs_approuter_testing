166286aa1070f59f88e212144957c027d617983a
NwaguChima <nwaguchima48@gmail.com>
2023-09-16 12:11:26 +0100

    updated test

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
-it('should have Docs text', () => {
-  render(<Home />); // ARRANGE
+describe('Home', () => {
+  it('should have Docs text', () => {
+    render(<Home />); // ARRANGE
 
-  const myElement = screen.getByText('Docs'); // ACT
+    const myElement = screen.getByText('Docs'); // ACT
 
-  expect(myElement).toBeInTheDocument(); // ASSERT
+    expect(myElement).toBeInTheDocument(); // ASSERT
+  });
+
+  it('should contain text "information"', () => {
+    render(<Home />); // ARRANGE
+
+    const myElement = screen.getByText(/information/i); // ACT
+
+    expect(myElement).toBeInTheDocument(); // ASSERT
+  });
 });
